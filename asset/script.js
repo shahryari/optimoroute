c0f40e2160b08a79d52e36630933e23c8c3fb262
Fatemeh <fatemeh.ganbari1993@gmail.com>
2024-05-18 14:31:07 +0330

    fix number of Market

--- asset/script.js
+++ asset/script.js
@@ -13,54 +13,42 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-// --------------------------------------------------
-// Nofiflix options
 
+// --------------------------------------------------
+// Notiflix options
 Notiflix.Notify.init({
     width: "280px",
     position: "right-bottom",
     distance: "10px",
 });
 // --------------------------------------------------
-// add buttons to map
-
+// Add buttons to map
 const customControl = L.Control.extend({
-    // button position
     options: {
         position: "topright",
     },
-
-    // method
     onAdd: function () {
         const array = [];
-
         const container = L.DomUtil.create(
             "div",
             "leaflet-control leaflet-action-button"
         );
-
         array.forEach((item) => {
             const button = L.DomUtil.create("a");
             button.href = "#";
             button.setAttribute("role", "button");
-            console.log(item.title);
             button.title = item.title;
             button.innerHTML = item.html;
             button.className += item.className;
-
-            // add buttons to container;
             container.appendChild(button);
         });
-
         return container;
     },
 });
 map.addControl(new customControl());
-// Drow polygon, circle, rectangle, polyline
-// --------------------------------------------------
 
+// Draw polygon, circle, rectangle, polyline
 let drawnItems = L.featureGroup().addTo(map);
-
 map.addControl(
     new L.Control.Draw({
         edit: {
@@ -82,19 +70,11 @@ map.on(L.Draw.Event.CREATED, function (event) {
     let layer = event.layer;
     let feature = (layer.feature = layer.feature || {});
     let type = event.layerType;
-
     feature.type = feature.type || "Feature";
     let props = (feature.properties = feature.properties || {});
-
     props.type = type;
-
     if (type === "circle") {
         props.radius = layer.getRadius();
-    }
-    if (type === "polygon" || type === "polyline") {
-        // Update the number of points in the marker icon
-        const latLngs = layer.getLatLngs();
-        const pointCount = latLngs.length;
     }
     drawnItems.addLayer(layer);
 });
@@ -108,7 +88,6 @@ tabs.forEach((tab) => {
     tab.addEventListener("click", function () {
         tabs.forEach((t) => t.classList.remove("active"));
         tabContents.forEach((tc) => tc.classList.remove("active"));
-
         this.classList.add("active");
         document.getElementById(this.dataset.tab).classList.add("active");
     });
@@ -154,29 +133,21 @@ var orders = [
         stopNumber: 2,
     },
 ];
+
 // Add a custom icon for numbered markers
-function createNumberedMarker(number) {
+function createNumberedMarker(number, isActive = false) {
     return L.divIcon({
-        className: "custom-icon",
-        html:
-            "<div class='content-icon'><i class='fa-solid fa-location-pin'></i><span class='icon-number'>" +
-            order.stopNumber +
-            "</span></div>",
+        className: isActive ? "custom-icon active-marker" : "custom-icon",
+        html: `<div class='content-icon'><i class='fa-solid fa-location-pin'></i><span class='icon-number'>${number}</span></div>`,
         iconSize: [30, 30],
         iconAnchor: [15, 30],
         popupAnchor: [0, -30],
     });
 }
-function createNumberedMarker(number) {
-    return L.divIcon({
-        className: "numbered-marker",
-        html: `<div class="numbered-marker-icon">${number}</div>`,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-    });
-}
+
 var orderTable = document.getElementById("orderTable");
 var markers = {}; // Store markers by order ID
+
 orders.forEach((order) => {
     var row = document.createElement("tr");
     row.setAttribute("id", "order-" + order.id);
@@ -192,25 +163,20 @@ orders.forEach((order) => {
     orderTable.appendChild(row);
 
     // Add marker to the map
-    var numMarker = L.divIcon({
-        className: "custom-icon",
-        html:
-            "<div class='content-icon'><i class='fa-solid fa-location-pin'></i><span class='icon-number'>" +
-            order.stopNumber +
-            "</span></div>",
-        iconSize: [30, 30],
-        iconAnchor: [15, 30],
-        popupAnchor: [0, -30],
-    });
     var marker = L.marker(order.address, {
-        icon: numMarker,
+        icon: createNumberedMarker(order.stopNumber),
     })
         .addTo(map)
         .on("click", clickZoom);
+
     marker.bindPopup(
         `<b>Order ID:</b> ${order.id}<br><b>Location:</b> ${order.location}<br><b>Driver:</b> ${order.driver}`
     );
+    function clickZoom(e) {
+        map.setView(e.target.getLatLng(), zoom);
 
+        setActive(e.target._leaflet_id);
+    }
     marker.on("mouseover", function (e) {
         this.openPopup();
     });
@@ -218,12 +184,7 @@ orders.forEach((order) => {
     marker.on("mouseout", function (e) {
         this.closePopup();
     });
-    // set center map
-    function clickZoom(e) {
-        map.setView(e.target.getLatLng(), zoom);
 
-        setActive(e.target._leaflet_id);
-    }
     markers[order.id] = marker; // Store marker by order ID
 
     row.addEventListener("click", function () {
@@ -232,46 +193,22 @@ orders.forEach((order) => {
         // Pan map to the marker
         map.setView(order.address, zoom);
     });
+
     // Add the coordinates to the latlngs array
     latlngs.push(order.address);
 });
 
 function highlightMarker(orderId) {
-    console.log(orderId, markers);
-
     // Reset all markers to default
     Object.values(markers).forEach((marker) => {
-        marker.setIcon(
-            L.divIcon({
-                className: "custom-icon",
-                html:
-                    "<div class='content-icon'><i class='fa-solid fa-location-pin'></i><span class='icon-number'>" +
-                    markers[orderId].options.icon.options.html.match(
-                        /<span class='icon-number'>(\d+)<\/span>/
-                    )[1] +
-                    "</span></div>",
-                iconSize: [30, 30],
-                iconAnchor: [15, 30],
-                popupAnchor: [0, -30],
-            })
-        );
+        const orderId = Object.keys(markers).find(id => markers[id] === marker);
+        const order = orders.find(order => order.id == orderId);
+        marker.setIcon(createNumberedMarker(order.stopNumber));
     });
 
     // Highlight the selected marker
-    markers[orderId].setIcon(
-        L.divIcon({
-            className: "custom-icon",
-            html:
-                "<div class='content-icon' ><i class='fa-solid fa-location-pin'></i><span class='icon-number active-marker'>" +
-                markers[orderId].options.icon.options.html.match(
-                    /<span class='icon-number'>(\d+)<\/span>/
-                )[1] +
-                "</span></div>",
-            iconSize: [30, 30],
-            iconAnchor: [15, 30],
-            popupAnchor: [0, -30],
-        })
-    );
+    const selectedOrder = orders.find(order => order.id == orderId);
+    markers[orderId].setIcon(createNumberedMarker(selectedOrder.stopNumber, true));
 }
 
 // Draw polyline between markers
